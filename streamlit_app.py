b3771a447c55783ea7b3933b700e9c731d021558
Zhe Guan <zhe.guan@cern.ch>
2024-11-27 01:52:07 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,14 @@ import numpy as np
 import pandas as pd
 import math
 from pathlib import Path
+from datetime import datetime
+import time
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import requests
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
@@ -16,49 +22,6 @@ st.set_page_config(
 # Declare some useful functions.
 
 @st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
 
 def check_symmtric(sorted_df):
     difference_no_nan = (sorted_df != sorted_df.transpose()) & sorted_df.notna() & sorted_df.transpose().notna()
@@ -69,6 +32,7 @@ def check_symmtric(sorted_df):
         original_value = sorted_df.loc[index]
         transpose_value = sorted_df.T.loc[index]
         print(f"Asymmetry at {index}: Original = {original_value}, Transpose = {transpose_value}")
+        return False
 
     return True
 
@@ -84,7 +48,99 @@ def find_pathes(previous, end_point):
             pathes.append(sub + [end_point])
     return pathes
 
-gdp_df = get_gdp_data()
+
+def calc_dis_path(n, start_point, end_point):
+    dist = [np.inf] * n  # define shortest distance
+    visited = [False] * n  # define visit status
+    previous = [[] for _ in range(n)]
+    dist[start_point] = 0
+
+    for _ in range(n):  # loop for every points we did not visit
+        u = -1
+        min_distance = inf
+
+        for i in range(n):
+            if not visited[i] and dist[i] < min_distance:
+                min_distance = dist[i]
+                u = i
+        if u == -1:
+            break
+
+        for j in range(n):
+            if visited[j] == False and np_network[u][j] != inf:
+                new_distance = dist[u] + np_network[u][j]  # where dist[u] is min_distance
+                print(u, j, new_distance, dist[u], np_network[u][j])
+                if new_distance < dist[j]:
+                    dist[j] = new_distance
+                    print("here we update: distance j ", j, " with ", new_distance)
+                    previous[j] = [u]
+                elif new_distance == dist[j]:
+                    previous[j].append(u)
+        visited[u] = True
+
+    return previous, dist
+
+
+def find_lowest_price1(city_pair="EDI-LHR", start_date=datetime.today().strftime('%Y-%m-%d')):
+    origin, destination = city_pair.split("-")
+
+    driver = webdriver.Chrome()
+
+    driver.get(f"https://www.kayak.com/flights/{origin}-{destination}/{start_date}?ucs=8liajb&sort=price_a")
+
+    time.sleep(20)
+
+    content = driver.page_source
+
+    soup = BeautifulSoup(content, 'html.parser')
+
+    prices = []
+    for div in soup.findAll('div', attrs={'class': 'f8F1-price-text'}):
+        price_text = div.text.strip()  # 提取价格文本
+        if price_text.startswith('$'):
+            price = int(price_text[1:])  # 去掉 "$" 并转换为整数
+            prices.append(price)
+    driver.quit()
+    if prices:
+        min_price = min(prices)
+        st.write(f"The minimum price of {city_pair} on {start_date} is: ${min_price}")
+        return min_price
+    else:
+        st.write(f"No prices found for {city_pair} on {start_date}")
+        return None
+
+
+
+def find_lowest_price(city_pair="EDI-LHR", start_date=datetime.today().strftime('%Y-%m-%d')):
+    origin, destination = city_pair.split("-")
+    url = f"https://www.kayak.com/flights/{origin}-{destination}/{start_date}?ucs=8liajb&sort=price_a"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
+
+    # 检查是否抓取成功
+    if response.status_code != 200:
+        st.write(f"Failed to retrieve data for {city_pair} on {start_date}")
+        return None
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    prices = []
+    for div in soup.findAll('div', attrs={'class': 'f8F1-price-text'}):
+        price_text = div.text.strip()
+        if price_text.startswith('$'):
+            price = int(price_text[1:])
+            prices.append(price)
+
+    if prices:
+        min_price = min(prices)
+        st.write(f"The minimum price of {city_pair} on {start_date} is: ${min_price}")
+        return min_price
+    else:
+        st.write(f"No prices found for {city_pair} on {start_date}")
+        return None
+
 
 # -----------------------------------------------------------------------------
 # Draw the actual page
@@ -163,7 +219,28 @@ network = {
 df = pd.DataFrame(network)
 sorted_df = df.sort_index(axis=0).sort_index(axis=1)
 
-check_symmtric(sorted_df)
+
+uploaded_file = st.file_uploader("Choose a CSV file to upload, otherwise default network will be used:", type="csv")
+upload_settings = st.checkbox(
+    "index names included",
+    help="Check this box when the index names are included in your csv files :)"
+)
+
+if uploaded_file is not None:
+    if upload_settings:
+        data = pd.read_csv(uploaded_file, index_col=0)
+    elif not upload_settings:
+        data = pd.read_csv(uploaded_file)
+        data = data.reset_index(drop=True)
+        data = data.drop(data.columns[0], axis=1)
+        data.columns = range(data.shape[1])
+
+    st.write("Uploaded CSV Data:")
+    st.dataframe(data)
+    sorted_df = data.sort_index(axis=0).sort_index(axis=1)
+    st.write("Check if uploaded network is bi-directional: ", check_symmtric(sorted_df))
+    if check_symmtric(sorted_df) is False:
+        st.write("Please double check the network uploaded which should be symmetrical :)")
 
 sorted_df = sorted_df.fillna(float('inf'))
 np_network = np.array(sorted_df) #transform to an array for better efficiency
@@ -171,7 +248,7 @@ np_network = np.array(sorted_df) #transform to an array for better efficiency
 #define varibales
 
 
-countries = sorted([i for i in network])
+countries = sorted([i for i in sorted_df.columns])
 
 if not len(countries):
     st.warning("Select at least one country")
@@ -189,120 +266,90 @@ end_station = st.selectbox(
 st.write(f'You selected the journey from **{start_station}** to **{end_station}**')
 
 n = len(np_network)
-dist = [np.inf] * n #define shortest distance
-visited = [False] * n #define visit status
-previous = [[] for _ in range(n)]
-#previous = [-1] * n #define mother point
 start_point = sorted_df.index.get_loc(start_station)
-dist[start_point] = 0
 end_point = sorted_df.index.get_loc(end_station)
 
-#st.write(start_point, "end: ", end_point )
-
-print(dist)
-print(visited)
-print(previous)
-
-
-# here start point = 3
-for _ in range(n):  # loop for every points we did not visit
-    u = -1
-    min_distance = inf
-
-    for i in range(n):
-        if not visited[i] and dist[i] < min_distance:
-            min_distance = dist[i]
-            u = i
-    if u == -1:
-        break
-
-    for j in range(n):
-        if visited[j] == False and np_network[u][j] != inf:
-            new_distance = dist[u] + np_network[u][j]  # where dist[u] is min_distance
-            print(u, j, new_distance, dist[u], np_network[u][j])
-            if new_distance < dist[j]:
-                dist[j] = new_distance
-                print("here we update: distance j ", j, " with ", new_distance)
-                previous[j] = [u]
-            elif new_distance == dist[j]:
-                previous[j].append(u)
-    visited[u] = True
-
+previous, dist = calc_dis_path(n, start_point, end_point)
 allpath = find_pathes(previous, end_point)
+with_min_transfer_path = []
+transfer_times = inf
 
-st.write("All Paths:")
-for path in allpath:
-    st.write(" -> ".join(map(str, sorted_df.columns[path])))
-    st.write(" The total travel time: ", dist[end_point], "h")
+st.write("**All Shortest Paths:**")
+for i, path in enumerate(allpath):
+    st.write(f"&nbsp;&nbsp;**{i}.** "+" -> ".join(map(str, sorted_df.columns[path])))
+    st.write(f"&nbsp;&nbsp;The total travel time: {dist[end_point]}h")
+    if len(allpath[i]) < transfer_times:
+        transfer_times =  len(allpath[i])
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
+st.write("---------------------------------------------------------------------------")
 
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
+with_min_transfer_path = [i for i in allpath if transfer_times == len(i)]
 
-
-    #gdp_df['Country Code'].unique()
-
-
-
-
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
+transfer_times_settings = st.checkbox(
+    "Prefer minimum number of transfers",
+    help="Check this box when you prefer the path with the minimum number of transfers :)"
 )
 
-''
-''
+if transfer_times_settings:
+    st.write("**BEST** Choice(s):")
+    for best_path in with_min_transfer_path:
+        st.write("->".join(map(str, sorted_df.columns[best_path])))
+
+lowest_price_settings = st.checkbox(
+    "Prefer lowest price with solid price",
+    help="Check this box when you prefer the lowest price to travel:)                                              "
+         "\n **NOTE: THIS IS TRAIL OPTION, PRICES IS SCRABED FROM KAYAK WEBSITE**"
+)
+
+airport_codes = {
+    "Edinburgh": "EDI",
+    "London": "LHR",
+    "Madrid": "MAD",
+    "Rome": "FCO",
+    "Budapest": "BUD",
+    "Kyiv": "KBP",
+    "Istanbul": "IST",
+    "Baku": "GYD",
+    "Donetsk": "DOK",
+    "Moscow": "SVO",
+    "Saint Petersburg": "LED",
+    "Stockholm": "ARN",
+    "Hamburg": "HAM",
+    "Tromsø": "TOS",
+    "Murmansk": "MMK",
+    "Arkhangelsk": "ARH",
+    "Ulyanovsk": "ULV",
+    "Bergen": "BGO",
+    "Oulu": "OUL"
+}
+
+under_war = ["KBP", "DOK", "SVO", "LED","MMK", "ARH", "ULV"]
+formatted_routes = []
+total_price = 0
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+progress_bar = st.progress(0)
+status = st.empty()
+if lowest_price_settings:
 
-st.header(f'GDP in {to_year}', divider='gray')
+    st.write("We are trying to find the lowest prices...")
+    for path in allpath:
+        cities = sorted_df.columns[path]
+        airport_list = [airport_codes[city] for city in cities]
+        st.write(f"&nbsp;&nbsp;**{i}.** " + " -> ".join(map(str, sorted_df.columns[path])))
+        if any(item in airport_list for item in under_war):
+            st.write("**ATTENTION:** Due to the ongoing war, some airports selected are currently unavailable in the function:(    "
+                     "\n:earth_asia: **Hope for world peace!!**")
+            break
+        for i in range(len(airport_list) - 1):
+            progress_bar.progress(i)
+            formatted_routes.append(f"{airport_list[i]}-{airport_list[i + 1]}")
+            total_price += find_lowest_price1(city_pair=f"{airport_list[i]}-{airport_list[i + 1]}")
+            progress_bar.progress(int(100*(i+1)/(len(airport_list) - 1)))
+            status.write(f" {100*(i+1)/(len(airport_list) - 1)}% complete")
 
-''
+        st.write("*Total prices* $",f"{total_price}")
 
-cols = st.columns(4)
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
 
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
